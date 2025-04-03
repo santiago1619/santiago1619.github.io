21fc3afd1b78b0923e8ae2424326d9da0b3aa60e
santiago1619 <santiago.rinconlopez@dcmail.ca>
2025-04-03 00:47:21 -0400

    adding other photos

--- LAB4/part2/gallery-start/gallery-start/main.js
+++ LAB4/part2/gallery-start/gallery-start/main.js
@@ -1,18 +1,43 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
 
-/* Declaring the alternative text for each image file */
+const imgArr = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
-/* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+const imgAlts = [
+    {altText: "Photo 1 - Mountain Trail Path"},
+    {altText: "Photo 2 - Mountain Peak and Trail"},
+    {altText: "Photo 3 - Orange Flowers"},
+    {altText: "Photo 4 - Desert Mountain View"},
+    {altText: "Photo 5 - Sunrise in the Desert"}
+  ];
 
-/* Wiring up the Darken/Lighten button */
+
+for (let i = 0; i < imgArr.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${imgArr[i]}`);
+    newImage.setAttribute('alt', imgAlts[i].altText);
+    thumbBar.appendChild(newImage);
+}
+
+
+thumbBar.addEventListener("click", (event) => {
+    displayedImage.setAttribute('src', event.target.getAttribute('src'));
+    displayedImage.setAttribute('alt', event.target.getAttribute('alt'));
+});
+
+
+btn.addEventListener("click", () => {
+    if (btn.getAttribute("class") === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+});
