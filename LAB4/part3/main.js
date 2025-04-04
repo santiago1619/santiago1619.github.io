c3ea92cdca11bd028e41ecd20fe85a124a1a0381
santiago1619 <santiago.rinconlopez@dcmail.ca>
2025-04-03 22:23:38 -0400

    got my first ball to apear:))))

--- LAB4/part3/main.js
+++ LAB4/part3/main.js
@@ -17,3 +17,25 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+class Ball {
+  constructor(x, y, velX, velY, color, size) { 
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+draw() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+
+}
+
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+testBall.draw()
